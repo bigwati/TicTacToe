0131e2339793d25dee4370c2e5caba5ff8ce2077
bigwati <jachymek.kuba@gmail.com>
2023-12-06 14:29:15 +0100

    added printBoard functionality

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class Board {
+    String[][] board;
+
+    public Board(String[][] board){
+        this.board = board;
+        Arrays.stream(this.board).forEach(a -> Arrays.fill(a, "[ ]"));
+    }
+
+    public void printBoard(){
+        for (String[] row : this.board) {
+            for (String value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        String[][] myBoard = new String[3][3];
+        Board myGame = new Board(myBoard);
+        myGame.printBoard();
+    }
 }
